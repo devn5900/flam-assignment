a670ac71c09e6e417352d00d988d1f9bfabff559
devn5900 <devn5900@gmail.com>
2023-09-22 08:58:13 +0530

    Overflow bug Fixed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import BottomTab from './components/BottomTab';
 
 function App() {
+  const [open,setOpen]= useState(false);
   return (
     <>
-    <div className='  bg-pink-200 '>
-      <div className=''>
-            <button>Open Sheet</button>
+    <div className=' flex flex-col gap-5 items-center justify-center'>
+            <h1 className='text-5xl font-bold text-center p-3'>Welcome to Flam's</h1>
+        <button onClick={()=>setOpen(!open)} className='py-2 px-5 bg-pink-800 text-sm font-bold text-white rounded-xl hover:bg-pink-600'>{open?"Close":"Open"} Sheet</button>
         </div>
+    <div  className='  bg-white '>
+      
+   {open&&<BottomTab setOpen={setOpen}  />}
     </div>
-   <BottomTab  />
     </>
   );
 }
--- src/components/BottomTab.jsx
+++ src/components/BottomTab.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useRef, useState } from 'react'
 import { motion, useScroll, useSpring } from 'framer-motion';
 
 
-const BottomTab = () => {
-    const [height,setHeight]= useState(50);
-    const [pointer,setPointer]= useState("pointer-events-none")
+const BottomTab = ({setOpen}) => {
+    const [height,setHeight]= useState(15);
+    const [pointer,setPointer]= useState(false)
     const [drag,setDrag]= useState(false);
     const [pageY,setPageY]= useState(0)
     const ih=window.innerHeight
@@ -26,9 +26,12 @@ const BottomTab = () => {
         refH.current = nh;
         console.log(nh);
         setPageY(e.pageY);
+        // setPointer(false);
+
     }
     const handleDragDown=(e)=>{
         setDrag(true);
+        setPointer(true);
         setPageY(e.pageY)
     }
     const handleDragUp=(e)=>{
@@ -37,21 +40,25 @@ const BottomTab = () => {
 
     }
     useEffect(()=>{
-        setTimeout(() => {
+       setTimeout(() => {
             setDrag(false)
         }, 1000);
     },[height])
     return (
         <>
-        <motion.div   className={`${drag?"pointer-events-auto":"pointer-events-none"}  bg-gray-300 transition-all duration-100 ease z-100  fixed top-0 left-0 w-full h-full flex items-center justify-end flex-col`}>
+        <motion.div   className={`${pointer?"pointer-events-auto":"pointer-events-none"}    transition-all duration-100 ease z-100  fixed top-0 left-0 w-full h-full flex items-center justify-end flex-col`}>
+          <div className='bg-gray-500 fixed z-10 opacity-[0.5] h-full w-full'></div>
            <motion.div
            transition={{delay:0.2}}
            animate={{height:`${height}%`}}
-            className=' transition-all duration-150 ease h-[50vh] px-4 py-3 lg:w-1/2 md:w-[50%] sm:w-[60%] rounded-tl-3xl rounded-tr-3xl bg-white'>
+            className=' fixed z-50 transition-all  duration-150 ease h-[20vh] px-4 py-3 lg:w-1/2 md:w-[50%] sm:w-[60%] rounded-tl-3xl rounded-tr-3xl bg-white'>
            <div onMouseMove={handleDragBtn} onMouseUp={handleDragUp} onMouseDown={handleDragDown }  className=' pointer-events-auto w-full flex justify-center cursor-grab  p-8'>
                 <span   className='w-[5rem] h-[5px] rounded-full  bg-gray-300 '> </span>
             </div>
-            <div  className='  overflow-y-auto h-full scrollbar '>
+            <div>
+                <span onClick={()=>setOpen(false)} className='p-3 w-8 h-8 rounded-full font-extrabold text-lg cursor-pointer bg-gray-300 text-center flex items-center justify-center float-right' >X</span>
+            </div>
+            <div  className='  overflow-y-auto h-full scrollbar  '>
                 <h1 className='text-5xl font-extrabold text-center pt-10'>Flam's Assignment</h1>
             </div>
            </motion.div>
